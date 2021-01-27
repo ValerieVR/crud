e5a47ebcb7d327a7f1b0b74f4c0fb3276a2d86d2
Valerie <akpotevalerie@gmail.com>
2021-01-27 07:46:46 +0100

    Require the setup file

--- Starter-pack/index.php
+++ Starter-pack/index.php
@@ -1,7 +1,9 @@
 <?php
+
+require 'setup.php';
+
 $books = $bookRepository->get();
 $bookRepository->create();
-
 
 // Load your view
 // Tip: you can load this dynamically and based on a variable, if you want to load another view
